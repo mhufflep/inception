58a26af38ea12657b5408fe4fb82795ae200b3a9
mhufflep <mhufflep@student.21-school.ru>
2022-09-25 16:05:02 -0700

    cadvisor & prometheus accessed only through nginx (HTTPS)

--- srcs/requirements/nginx/tools/run.sh
+++ srcs/requirements/nginx/tools/run.sh
@@ -3,7 +3,7 @@
 CONF_PATH='/etc/nginx/http.d/default.conf'
 
 if [ ! -e $CONF_PATH ]; then
-    cat /default.conf.tmpl | envsubst '$NGINX_PORT $DOMAIN_NAME $CRT_PATH $KEY_PATH $PV_WP_MOUNT_PATH $PV_CV_MOUNT_PATH $PV_ADM_MOUNT_PATH $ADMINER_PORT $WORDPRESS_PORT' > $CONF_PATH
+    cat /default.conf.tmpl | envsubst '$NGINX_PORT $DOMAIN_NAME $CRT_PATH $KEY_PATH $PV_WP_MOUNT_PATH $PV_CV_MOUNT_PATH $PV_ADM_MOUNT_PATH $ADMINER_PORT $WORDPRESS_PORT $PROMETHEUS_PORT $CADVISOR_PORT' > $CONF_PATH
 fi
 
 exec nginx -g 'daemon off;' $@
